aec36d6f58f6a6f60dcd537e09f620fa1cb94c6e
gabriella.gricius@gmail.com <gabriella.gricius@gmail.com>
2017-02-02 12:37:05 +0100

    added price column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202102641) do
+ActiveRecord::Schema.define(version: 20170202105829) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170202102641) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.string   "price"
+    t.string   "decimal"
   end
 
 end
