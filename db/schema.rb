f7c9b75bdd984a3893787b0ce5e3673a8164a871
gabriella.gricius@gmail.com <gabriella.gricius@gmail.com>
2017-02-06 20:37:00 +0100

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,7 @@ ActiveRecord::Schema.define(version: 20170204104610) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
-    t.string   "price"
-    t.string   "decimal"
+    t.decimal   "price"
   end
 
 end
