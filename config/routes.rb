50e358354c5344c9dd014820f9ddb5a29e202821
gabriella.gricius@gmail.com <gabriella.gricius@gmail.com>
2017-02-14 22:17:58 +0100

    5.4 user authenitication and password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+  resources :products
+  resources :users
+  
   get 'static_pages/about'
   get 'static_pages/contact'
   post 'static_pages/thank_you'
   get 'static_pages/index'
   get 'static_pages/landing_page'
   root 'static_pages#contact'
-  resources :orders, only: [:index, :show, :create, :destroy]
-
-  resources :products
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
