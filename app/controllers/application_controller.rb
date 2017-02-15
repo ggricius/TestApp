c2504d4e46496d4ce01ec0a705afcd874dbdd00d
gabriella.gricius@gmail.com <gabriella.gricius@gmail.com>
2017-02-15 21:03:49 +0100

    5.5 commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from "CanCan::AccessDenied" do |exception|
-  redirect_to main_app.root_url, :alert => exception.message
+  	redirect_to main_app.root_url, :alert => exception.message
+	end
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    update_attrs = [:password, :password_confirmation, :current_password]
+    devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+
+  #rescue_from "CanCan::AccessDenied" do |exception|
+  #redirect_to main_app.root_url, :alert => exception.message
 end
 end
