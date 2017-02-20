fdbed5296c570ab8d42b23cbcdfc5ae9f44ae370
gabriella.gricius@gmail.com <gabriella.gricius@gmail.com>
2017-02-20 12:18:33 +0100

    5.5 edit user

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
 
   resources :products
   resources :users
-  
+
   get 'static_pages/about'
   get 'static_pages/contact'
   post 'static_pages/thank_you'
   get 'static_pages/index'
   get 'static_pages/landing_page'
-  root 'static_pages#contact'
+  root 'static_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
