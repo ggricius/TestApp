46e64491b0887f2ae4575ac77eb534e9b5dccc1b
gabriella.gricius@gmail.com <gabriella.gricius@gmail.com>
2017-02-23 21:58:04 +0100

    5.8 commentary issues

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,14 @@ class Product < ApplicationRecord
   has_many :comments
 
   def highest_rating_comment
-  comments.rating_desc.first
+    comments.rating_desc.first
   end
 
   def lowest_rating_comment
-  comments.rating_asc.first
+    comments.rating_asc.first
+  end
+
+  def average_rating
+    comments.average(:rating).to_f
   end
 end
