a7f02946e4c59ca23a8c738a415dbdedc5b92110
gabriella.gricius@gmail.com <gabriella.gricius@gmail.com>
2017-03-08 18:58:04 +0100

    Taking out Scaffolding and Pagination and Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,8 @@
 class Product < ApplicationRecord
+
+  validates :name, presence: true
   has_many :orders
   has_many :comments
-
-  validates :body, presence: true
-  validates :user, presence: true
-  validates :product, presence: true
-  validates :rating, numericality: { only_integer: true }
 
   def highest_rating_comment
     comments.rating_desc.first
