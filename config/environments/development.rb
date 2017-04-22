efb57e0140ae7d009c0f3888119e3d61a7d79a22
gabriella.gricius@gmail.com <gabriella.gricius@gmail.com>
2017-04-22 19:51:41 +0200

    6.13 again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,8 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.app_generators.javascript_engine = :javascript
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
